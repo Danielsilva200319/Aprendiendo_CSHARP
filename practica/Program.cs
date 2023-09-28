d434aa295bae36fbfc466e8ca13081776cc2515c
Daniel silva <89894235+Danielsilva200319@users.noreply.github.com>
2023-09-28 11:25:20 +0000

    act

---
+++ practica/Program.cs
@@ -0,0 +1,18 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        // Programa que calcula el área de un rectangulo.
+        int LadoA;
+        int LadoB;
+        int resultado;
+
+        Console.WriteLine("Calcula el área de un rectangulo");
+        Console.Write("\nIngrese el valor del lado A: ");
+        LadoA = Convert.ToInt32(Console.ReadLine());
+        Console.Write("\nIngrese el valor del lado B: \n");
+        LadoB = Convert.ToInt32(Console.ReadLine());
+        resultado = LadoA * LadoB;
+        Console.WriteLine("El resultado es: " + resultado);
+    }
+}
