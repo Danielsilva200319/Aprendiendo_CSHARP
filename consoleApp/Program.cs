de528352d52320436b7f6b53621633652668b7e7
Daniel silva <89894235+Danielsilva200319@users.noreply.github.com>
2023-10-11 12:41:48 +0000

    😇

---
+++ consoleApp/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Runtime.InteropServices;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.Write("Por favor ingrese un nombre: ");
+        var nombre = Console.ReadLine();
+        Console.Write("Por favor ingrese su cargo: ");
+        var cargo = Console.ReadLine();
+        Console.Write("Por favor ingrese su edad: ");
+        var edad = Console.ReadLine();
+        Console.WriteLine($"\nMi nombre es {nombre}");
+        Console.WriteLine($"Mi cargo es {cargo}");
+        Console.WriteLine($"Mi edad es {edad}");
+    }
+}
